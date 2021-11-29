31ff0db88d9b6df5cfb0a482999a1595b5a53c3b
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2021-11-29 17:31:08 +0100

    Created HTML structure and initial CSS styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
+        <footer>Created by Andrea Roldán, open-sourced on GitHub</footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,53 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-    return (
-        <div>hello the grass is green</div>
-    )
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-info w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>City Name</h1>
+      <ul>
+        <li>Monday 16:11</li>
+        <li>Sunny with a chance of clouds</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/snow_light.png"
+              alt="Sunny with a chance"
+              className="float-left"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">º C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation</li>
+            <li>Humidity</li>
+            <li>Wind</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
