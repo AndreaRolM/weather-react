cfb05b475edfb193f04f55f13bb4671429b35da2
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2021-11-29 15:40:49 +0100

    Polished everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import "./App.css";
 import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Weather search</h2>
         <Search />
+      </header>
+      <footer>
+        This project was coded by Andrea Roldán and it is{" "}
         <a
           className="App-link"
           href="https://github.com/AndreaRolM?tab=repositories"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Take a look to GitHub repository
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          className="App-link"
+          href="https://clever-lamarr-1c94b2.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
