49cdf200b6879159be27eb20471cc2bdab7e6cad
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2021-11-29 16:00:19 +0100

    Set up weather component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import "./App.css";
-import Search from "./Search";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Weather search</h2>
-        <Search />
-      </header>
-      <footer>
-        This project was coded by Andrea Roldán and it is{" "}
-        <a
-          className="App-link"
-          href="https://github.com/AndreaRolM?tab=repositories"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          className="App-link"
-          href="https://clever-lamarr-1c94b2.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   );
 }
