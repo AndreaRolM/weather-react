c6eae79b0c7ff643357a97970941f791805fecb5
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2021-12-01 15:51:12 +0100

    Set axios and API call up

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search city"
-              className="form-control"
-              autoFocus="on"
-            />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-info w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-info w-100"
-            />
+        </form>
+        <h1>City Name</h1>
+        <ul>
+          <li>Monday 16:11</li>
+          <li>Sunny with a chance of clouds</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/snow_light.png"
+                alt="Sunny with a chance"
+                className="float-left"
+              />
+              <span className="temperature">{Math.round(temperature)}</span>
+              <span className="unit">º C</span>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>City Name</h1>
-      <ul>
-        <li>Monday 16:11</li>
-        <li>Sunny with a chance of clouds</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/snow_light.png"
-              alt="Sunny with a chance"
-              className="float-left"
-            />
-            <span className="temperature">6</span>
-            <span className="unit">º C</span>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 km/h</li>
+            </ul>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation</li>
-            <li>Humidity</li>
-            <li>Wind</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "0acaa1de783699a7c805c0e5bad91ab8";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
