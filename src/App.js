c6fd508c1378de03dd4418cc903b121100c0fddd
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2021-12-03 14:39:31 +0100

    Included rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -11,15 +11,24 @@ export default function App() {
           <a
             href="https://www.linkedin.com/in/andrea-rold%C3%A1n-mohedano-242461158/"
             target="_blank"
+            rel="noreferrer"
           >
             Andrea Roldán
           </a>
           , open-sourced on{" "}
-          <a href="https://github.com/AndreaRolM/weather-react" target="_blank">
+          <a
+            href="https://github.com/AndreaRolM/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub
           </a>{" "}
           and hosted on{" "}
-          <a href="https://clever-lamarr-1c94b2.netlify.app/" target="_blank">
+          <a
+            href="https://clever-lamarr-1c94b2.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Netlify
           </a>
           .
