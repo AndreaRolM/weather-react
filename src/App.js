c39680a88ca1cb9bedde946d664600f01391b326
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2021-12-03 14:34:57 +0100

    Added footer links

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="New York" />
-        <footer>Created by Andrea Roldán, open-sourced on GitHub</footer>
+        <footer>
+          Created by{" "}
+          <a
+            href="https://www.linkedin.com/in/andrea-rold%C3%A1n-mohedano-242461158/"
+            target="_blank"
+          >
+            Andrea Roldán
+          </a>
+          , open-sourced on{" "}
+          <a href="https://github.com/AndreaRolM/weather-react" target="_blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://clever-lamarr-1c94b2.netlify.app/" target="_blank">
+            Netlify
+          </a>
+          .
+        </footer>
       </div>
     </div>
   );
