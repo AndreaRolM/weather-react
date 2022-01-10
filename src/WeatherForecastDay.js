5f6ef4f5cca94840515dbcf08d1f011461cc82b8
AndreaRolM <82528161+AndreaRolM@users.noreply.github.com>
2022-01-10 11:24:09 +0100

    Replaced fake data with API real values

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,13 +2,35 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-    return (
-      <div className="WeatherForecastDay">
-        <div className="WeatherForecast-day">Tue</div>
-        <WeatherIcon code="01d" size={32} />
-        <div className="WeatherForecast-temperatures"></div>
-        <span className="WeatherForecast-temperatures-max">19º</span>
-        <span className="WeatherForecast-temperatures-min">10º</span>
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}º`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}º`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="WeatherForecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={32} />
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-temperatures-max">
+          {maxTemperature()}
+        </span>
+        <span className="WeatherForecast-temperatures-min">
+          {minTemperature()}
+        </span>
       </div>
-    );
+    </div>
+  );
 }
